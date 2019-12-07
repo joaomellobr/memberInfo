829de6d280dfc341bc2b866d18047c1056910129
joaomellobr <joaomellobr@gmail.com>
2019-12-06 23:38:55 -0500

    Create put.js

---
+++ put.js
@@ -0,0 +1,40 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    let responseBody = "";
+    let statusCode = 0;
+
+    const { id, membername, memberemail } = JSON.parse(event.body);
+
+    const params = {
+        TableName: "Members",
+        Item: {
+            id: id,
+            membername: membername,
+            memberemail: memberemail
+        }
+    }
+
+    try {
+        const data = await documentClient.put(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 201;
+    } catch (err) {
+        responseBody = `Unable to put member: ${err}`;
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "access-control-allow-origin": "*"
+        },
+        body: responseBody
+    };
+
+    return response;
+}
